ee4b859985928fca093cc53d4ec4e091ae1b61aa
PR0G4M3R <pr0g4m3r.2023@gmail.com>
2025-08-01 19:29:07 -0700

    Delete Admin Tools - Mid - Copy directory

--- Admin Tools - Mid - Copy/tools/SystemBackup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Creating System Restore Point...
-powershell -command "Checkpoint-Computer -Description 'SystemBackup Tool' -RestorePointType 'MODIFY_SETTINGS'"
-
-echo Backing up key folders...
-xcopy "%USERPROFILE%\Documents" "%SystemDrive%\Backup\Documents" /E /I /Y
-xcopy "%USERPROFILE%\Pictures" "%SystemDrive%\Backup\Pictures" /E /I /Y
-
-echo Backup complete. Folders saved to %SystemDrive%\Backup
